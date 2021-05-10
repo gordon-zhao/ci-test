eab6bc7c7030a9da285f8e6e6a98850a600995b8
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-05-11 00:14:52 +0800

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
   await page.goto('http://localhost');
   console.log("Hey Console!");
   await page.click(".test_button");
-  await test('Did I click the button?', () => {
+  test('Did I click the button?', () => {
       expect.anything(document.getElementById("test_content"))
   })
   await browser.close();
