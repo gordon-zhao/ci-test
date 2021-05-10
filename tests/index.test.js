44a851f9c09bb7e3e339e65664f1195fac2f7670
gordon-zhao <gordon-zhao@users.noreply.github.com>
2021-05-11 01:52:13 +0800

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -20,8 +20,7 @@ describe('Test the test_button', () => {
   it('Did I just click the button?', async() => {
       await page.click("#test_button");
       let result = await page.evaluate(() => {
-        console.log(document.getElementById("test_content"));
-        return document.getElementById("test_content");
+        return document.getElementById("test_content").textContent;
       });
       console.log(await result);
       await expect.anything(result);
